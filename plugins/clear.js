e0ea46a7a9f2c538b53a7a2d936a8bc3fb81ecaf
Project-TH <102356993+Project-TH@users.noreply.github.com>
2022-03-27 23:06:36 +0530

    Create clear.js

---
+++ plugins/clear.js
@@ -0,0 +1,22 @@
+const {MessageType, GroupSettingChange, ChatModification, WAConnectionTest} = require('@adiwajshing/baileys');
+const Neotro = require('../events');
+const Config = require('../config');
+const Language = require('../language');
+const Lang = Language.getString('nothing');
+const mut = Language.getString('mute');
+const END = "Clear Chat"
+
+async function checkImAdmin(message, user = message.client.user.jid) {
+    var grup = await message.client.groupMetadata(message.jid);
+    var sonuc = grup['participants'].map((member) => {
+        if (member.id.split('@')[0] === user.split('@')[0] && member.isAdmin) return true; else; return false;
+    });
+    return sonuc.includes(true);
+}
+
+Asena.addCommand({pattern: 'clear', fromMe: true, desc: Lang.CLEAR, dontAddCommandList: false}, (async (message, match) => {
+
+    await message.sendMessage('Clearing Chat...');
+    await message.client.modifyChat (message.jid, ChatModification.delete);
+    await message.sendMessage('Chat Cleared!');
+}));
