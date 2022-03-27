456945bf8a5e02e9f555ee7fb67e2a0f95cc76bd
Project-TH <102356993+Project-TH@users.noreply.github.com>
2022-03-27 21:37:31 +0530

    Create fetcher.js

---
+++ fetcher.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch')
+
+exports.fetchJson = fetchJson = (url, options) => new Promise(async(resolve, reject) => {
+    fetch(url, options)
+        .then(response => response.json())
+        .then(json => {
+            resolve(json)
+        })
+        .catch((err) => {
+            reject(err)
+        })
+})
+
+
+exports.fetchText = fetchText = (url, options) => new Promise(async(resolve, reject) => {
+    fetch(url, options)
+        .then(response => response.text())
+        .then(text => {
+            resolve(text)
+        })
+        .catch((err) => {
+            reject(err)
+        })
+})
