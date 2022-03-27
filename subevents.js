6de64544c1c6dd5727cf7fea9378a327dc3f0163
Project-TH <102356993+Project-TH@users.noreply.github.com>
2022-03-27 21:48:29 +0530

    Update subevents.js

--- subevents.js
+++ subevents.js
@@ -1,15 +1,3 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
-*/
-/*
-Sub-Events for WhatsAsena
-Experimental Datas
-*/
-
 var config = require('./config');
 var Commands = [];
 
